90cd7988493ea25fcd8da474f335d58fe4db27f1
33-Shikamaru <jeffersonmclinden@gmail.com>
2025-05-04 21:37:59 -0700

    Feat: added basic routing to pages on navbar and sign out button

--- poketcgtrading/src/App.js
+++ poketcgtrading/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/sign-in/landing';
+// import Inventory from './pages/inventory/inventory'
 // import Signup from './pages/sign-in/signup';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/inventory" element={<Navbar />} />
+
         {/* <Route path="/signup" element={<div>Sign Up Page</div>} /> */}
       </Routes>
     </Router>
--- poketcgtrading/src/components/navbar.jsx
+++ poketcgtrading/src/components/navbar.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { TbBellRinging } from "react-icons/tb";
 import { FaRegUserCircle } from "react-icons/fa";
 import TCGLogo from "../assets/tcgLogo.png";
 
-const Navbar = () => (
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
     <nav className='relative flex items-center justify-between border-b border-gray-400 px-4'>
       <img className='flex items-center' src={TCGLogo} width={80}/>
 
       <div className='absolute left-1/2 transform -translate-x-1/2 flex space-x-6'>
       {/* <div className='justify-center flex row gap-3'> */}
-          <button className='rounded p-1 hover:bg-gray-200'>Inventory</button>
-          <button className='rounded p-1 hover:bg-gray-200'>Wish List</button>
-          <button className='rounded p-1 hover:bg-gray-200'>Friends</button>
-          <button className='rounded p-1 hover:bg-gray-200'>Trade</button>
-          <button className='rounded p-1 hover:bg-gray-200'>Explore</button>
+          <button className='rounded p-1 hover:bg-gray-200' onClick={() => navigate('/inventory')}>Inventory</button>
+          <button className='rounded p-1 hover:bg-gray-200' onClick={() => navigate('/wishlist')}>Wish List</button>
+          <button className='rounded p-1 hover:bg-gray-200' onClick={() => navigate('/friends')}>Friends</button>
+          <button className='rounded p-1 hover:bg-gray-200' onClick={() => navigate('/trade')}>Trade</button>
+          <button className='rounded p-1 hover:bg-gray-200' onClick={() => navigate('/explore')}>Explore</button>
       </div>
 
       <div className='flex gap-2'>
@@ -24,9 +28,11 @@ const Navbar = () => (
           <button className='items-center rounded hover:bg-gray-200 text-2xl p-2'><FaRegUserCircle /></button>
 
           {/* Sign Out Button */}
-          <button className='rounded-xl bg-black text-white p-0.5 px-3 hover:bg-gray-500 text-sm'>Sign Out</button>
+          <button className='rounded-xl bg-black text-white p-0.5 px-3 hover:bg-gray-500 text-sm'
+            onClick={() => navigate('/')}
+          >Sign Out</button>
       </div>
     </nav>
-);
+)};
 
 export default Navbar;
