da2b9d15a7a1288c20fd84c247d917a8092c98c4
33-Shikamaru <jeffersonmclinden@gmail.com>
2025-05-15 16:24:28 -0700

    Fix: repositioned the dropdown menus to center for mobile devices

--- poketcgtrading/src/pages/inventory/inventory.jsx
+++ poketcgtrading/src/pages/inventory/inventory.jsx
@@ -13,27 +13,27 @@ function Inventory() {
     }];
 
   return (
-    <div className='min-h-screen'>
-      <div className='w-full max-w-lg mx-5 my-5'>
+    <div className='min-h-screen w-full overflow-x-hidden'>
+      <div className='w-full max-w-lg mx-auto px-4 my-5'>
         <h1 className='text-4xl font-bold pb-5'>Inventory</h1>
       </div>
       {/* Body Inventory Container */}
-      <div className='w-full'>
-        <div className='flex flex-row justify-center items-center pb-1 mx-5 gap-5 text-md'>
-          <div className='flex flex-row justify-center items-center gap-2'>
-            <p>Platform:</p>
+      <div className='w-full px-4'>
+        <div className='flex flex-col sm:flex-row justify-center items-center pb-1 gap-2 sm:gap-5 text-md'>
+          <div className='flex flex-row justify-center items-center gap-1 sm:gap-2 w-3/4 sm:w-auto'>
+            <p className='w-full text-center sm:text-left'>Platform:</p>
             <Dropdown typeFilter="platform" />
           </div>
-          <div className='flex flex-row justify-center items-center gap-2'>
-            <p>Sort By:</p>
+          <div className='flex flex-row justify-center items-center gap-1 sm:gap-2 w-3/4 sm:w-auto'>
+            <p className='w-full text-center sm:text-left'>Sort By:</p>
             <Dropdown typeFilter="sort" />
           </div>
-          <div className='flex flex-row justify-center items-center gap-2'>
-            <p>Filter By:</p>
+          <div className='flex flex-row justify-center items-center gap-1 sm:gap-2 w-3/4 sm:w-auto'>
+            <p className='w-full text-center sm:text-left'>Filter By:</p>
             <Dropdown typeFilter="filter" />
           </div>
         </div>
-        <div className='flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 rounded-lg p-2 mx-5'>
+        <div className='flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 rounded-lg p-2'>
           <div className='flex flex-col gap-4'>
             {/* Cards Loaded from Firebase Wish List */}
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
@@ -49,15 +49,15 @@ function Inventory() {
                     />
                     <div className='flex flex-col justify-between items-center'>
                       <div className="p-3">
-                          <h3 className="font-semibold text-gray-800">Sample</h3>
-                          <p className="text-sm text-gray-600">Test</p>
+                          <h3 className="font-semibold text-gray-800">{card.name}</h3>
+                          <p className="text-sm text-gray-600">{card.set}</p>
                       </div>
                     </div>
                   </div>
               ))}
             </div>
           </div>
-        </div> 
+        </div>
       </div>
     </div>
   )
